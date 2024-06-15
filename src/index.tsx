e961dafc09fcd7aff0d043b9e560b6d197911b4a
Danila Volkov <123877021+noxlovette@users.noreply.github.com>
2024-06-15 17:50:38 +0200

    auth command line

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,51 @@
 import Bun from "bun";
+import { TelegramClient } from 'telegram';
+import { StringSession } from 'telegram/sessions';
+
+const SESSION = new StringSession('');
+const API_ID: number = parseInt(process.env.APP_ID || '', 10);
+const API_HASH: string = process.env.APP_HASH || '';
+
+const client = new TelegramClient(SESSION, API_ID, API_HASH, {connectionRetries: 5})
 
 const server = Bun.serve({
   async fetch(req) {
     const path = new URL(req.url).pathname;
-  
 
-    if (path === "/auth" && req.method == "POST") {
-      try {
-        const body = await req.json();
-        console.log(body)
-        const reponse = new Response(JSON.stringify(body), {
-          status:200,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+   if (path === "/send-code" && req.method === "POST") {
+    console.log("Sending code");
+    const {phoneNumber} = await req.json();
 
-        return reponse
-      } catch (error) {
-        return new Response("Failed to fetch data from TG", {status: 500})
-      }
-    }
+try {
+await client.connect();
+await client.sendCode({apiId: API_ID, apiHash: API_HASH}, phoneNumber);
+return new Response("Code Sent")
+} catch (error) {
+  console.error("Error sending code: ", error);
+  return new Response("Error", {status: 500});
+}
+}
+if (path ==="/start-client" && req.method === "POST") {
+  const {phoneNumber, phoneCode} = await req.json();
 
-    // Handle POST request to /echo
-    if (path === "/echo" && req.method === "POST") {
-      try {
-        const body = await req.json(); // Parse request body as JSON
-        console.log("Received JSON:", body);
-        const { secret, receiver } = body; // Destructure properties from body
-  
-        // Echo back the received JSON
-        const response = new Response(JSON.stringify(body), {
-          status: 200,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-  
-        return response;
-      } catch (error) {
-        console.error("Error processing request:", error);
-        return new Response("Failed to process request", { status: 500 });
-      }
-    }  
+  try {
+    await client.start({
+      phoneNumber,
+      phoneCode: async () => phoneCode, 
+      onError: (err) => console.log("Error logging in: ", err)
+    });
+    await client.sendMessage('me', {message: "WE ARE IN"});
+
+    const savedSession = client.session.save();
+
+  return new Response(JSON.stringify(savedSession), {
+  headers: {"Content-Type": "application/json"},
+});
+  } catch (error) {
+    console.log("Error starting client: ", error);
+    return new Response("Error", {status: 500});
+  }
+}
 
     // Respond with other routes
     if (path === "/") {
